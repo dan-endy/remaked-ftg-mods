a51dc8084a3f775b684a12c859ed1402e47c7a5f
denolivka <50826947+denolivka@users.noreply.github.com>
2022-06-06 12:05:46 +0300

    Add files via upload

---
+++ reversekurva.py
@@ -0,0 +1,41 @@
+__version__ = (0,0,2)
+#meta developer: "скилз еблан, держу в курсе"
+from .. import loader, utils
+from telethon.tl.types import Message
+import logging
+
+logger = logging.getLogger(__name__)
+
+def mother_in_konava(message: Message) -> str:
+    if not (message := getattr(message, "message", message)):
+        return False
+    if len(args := message.split(maxsplit=1)) > 1:
+        return args[0]+" "+args[1]
+    return ""
+
+class ReverseTextMod(loader.Module):
+	strings = {
+		"name": "Reverse",
+	}
+	def __init__(self):
+		self.config = loader.ModuleConfig(
+			loader.ConfigValue(
+				"encoding",
+				"none",
+				lambda: "code, u, em, del это варианты вида текста, введите одно из них и увидите результат. 'none' что бы убрать кодировку",
+				validator=loader.validators.Choice(["code", "u", "em", "del", "none"]),
+				),
+			)
+
+	async def revcmd(self, message: Message):
+		reply = await message.get_reply_message()
+		try:
+			args = mother_in_konava(reply)
+			rev = list(args)
+			rev.reverse()
+			await utils.answer(message, f'{"".join(rev)}')
+		except TypeError:
+			args = utils.get_args_raw(message)
+			rev = list(args)
+			rev.reverse()
+			await utils.answer(message, f'{"".join(rev)}')
